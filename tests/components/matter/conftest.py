e36ee8d2152ef596205f8338ce8a0be65f8f2a82
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-10-02 14:20:09 +0000

    Add tests

--- tests/components/matter/conftest.py
+++ tests/components/matter/conftest.py
@@ -123,6 +123,7 @@ async def integration_fixture(
         "switch_unit",
         "temperature_sensor",
         "thermostat",
+        "thermostat-unoccupied",
         "vacuum_cleaner",
         "valve",
         "window_covering_full",
